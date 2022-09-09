c5e86485730c39706b5bc4f51059c92990dd84b0
bio-boris <bio-boris@users.noreply.github.com>
2022-09-09 10:54:52 -0500

    Update start-condor.sh

--- deployment/bin/start-condor.sh
+++ deployment/bin/start-condor.sh
@@ -6,4 +6,8 @@ if [ "$POOL_PASSWORD" ] ; then
     /usr/sbin/condor_store_cred -p "$POOL_PASSWORD" -f `condor_config_val SEC_PASSWORD_FILE`
 fi
 
+if [ "$CONDOR_CONFIG_FILEPATH" ] ; then
+    cp $CONDOR_CONFIG_FILE /etc/condor/condor_config.local
+fi
+
 $(condor_config_val MASTER) -f -t
