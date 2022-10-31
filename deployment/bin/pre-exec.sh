ab7d1f3891ce9c7dc7bbd99cfd562746e82e88f7
bio-boris <bio-boris@users.noreply.github.com>
2022-10-31 18:25:37 -0500

    Update pre-exec.sh

--- deployment/bin/pre-exec.sh
+++ deployment/bin/pre-exec.sh
@@ -5,6 +5,7 @@
 # If there is an environment variable "POOL_PASSWORD" write it out to the pool
 # condor pool password
 if [ "$POOL_PASSWORD" ] ; then
+    mkdir /etc/condor/passwords-orig.d/
     /usr/sbin/condor_store_cred -p "$POOL_PASSWORD" -f /etc/condor/passwords-orig.d/pool_password_from_env
 fi
 
