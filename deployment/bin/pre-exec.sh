53a11132de6b5d7c9255e96ef40c5870610a5689
bio-boris <bio-boris@users.noreply.github.com>
2022-11-02 17:00:11 -0500

    Update pre-exec.sh

--- deployment/bin/pre-exec.sh
+++ deployment/bin/pre-exec.sh
@@ -9,6 +9,15 @@ if [ "$POOL_PASSWORD" ] ; then
     /usr/sbin/condor_store_cred -p "$POOL_PASSWORD" -f /etc/condor/passwords-orig.d/pool_password_from_env
 fi
 
+# JWT Token created by `condor_token_create -identity condor-central-manager`
+if [ "$JWT_TOKEN"] ; then
+    echo "$JWT_TOKEN" >  /etc/condor/tokens-orig.d/jwt_from_env
+fi
+
 if [ "$CONDOR_CONFIG_FILEPATH" ] ; then
     cp "$CONDOR_CONFIG_FILEPATH" /etc/condor/condor_config.local
+fi
+
+if [ "$SCHEDD_HOST" ] ; then
+    echo "DAEMON_LIST = MASTER SCHEDD COLLECTOR NEGOTIATOR" > /etc/condor/config.d/daemon_list.conf
 fi
