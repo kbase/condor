94187ab79f2a65f433e23f4147812df568bfdf2f
bio-boris <bio-boris@users.noreply.github.com>
2022-12-09 13:09:52 -0600

    Update pre-exec.sh

--- pre-exec.sh
+++ pre-exec.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # See documentation at https://github.com/htcondor/htcondor/tree/main/build/docker/services#providing-additional-configuration
 
 # If there is an environment variable "POOL_PASSWORD" write it out to the pool
@@ -9,22 +8,10 @@ if [ "$CONDOR_SIGNING_KEY" ] ; then
     chmod 600 /etc/condor/passwords.d/POOL
 fi
 
-if [ "$CONDOR_JWT_TOKEN" ] ; then
-    echo "$CONDOR_JWT_TOKEN" > /etc/condor/tokens.d/JWT
-    chmod 600 /etc/condor/tokens.d/JWT
-fi
-
-# Hacks to enable worker for testing using this same image
-if [ "$COLLECTOR_HOST" ] ; then
-    echo "COLLECTOR_HOST = $COLLECTOR_HOST" >> /etc/condor/condor_config.local
-    echo "DAEMON_LIST = MASTER STARTD" >> /etc/condor/condor_config.local
-    /update-config
-fi
-
-# Allow StartD at kbase and nersc
+# Allow Starting up workers, and submitting jobs to queue 
 if [ "$UID_DOMAIN" ] ; then
     echo "ALLOW_ADVERTISE_STARTD = \$(ALLOW_ADVERTISE_STARTD) kbase_workers@${UID_DOMAIN} nersc_workers@${UID_DOMAIN}" >> /etc/condor/condor_config.local
-    echo "ALLOW_WRITE = $(authuser)@$(FULL_HOSTNAME) $(authuser)@$(IP_ADDRESS) kbase_workers@${UID_DOMAIN} nersc_workers@${UID_DOMAIN}" >> /etc/condor/condor_config.local  
+    echo "ALLOW_WRITE = \$(ALLOW_WRITE) kbase_workers@${UID_DOMAIN} nersc_workers@${UID_DOMAIN}" >> /etc/condor/condor_config.local  
     /update-config
 fi
 
@@ -32,4 +19,20 @@ fi
 if [ "$OVERWRITE_CONFIG_FILEPATH" ] ; then
     cp "$CONDOR_CONFIG_FILEPATH" /etc/condor/condor_config.local
     /update-config
+fi
+
+
+
+# FOR TESTING PURPOSES ONLY
+# Hacks to enable worker for testing using this same image
+if [ "$COLLECTOR_HOST" ] ; then
+    echo "COLLECTOR_HOST = $COLLECTOR_HOST" >> /etc/condor/condor_config.local
+    echo "DAEMON_LIST = MASTER STARTD" >> /etc/condor/condor_config.local
+    
+    /update-config
+
+    if [ "$CONDOR_JWT_TOKEN" ] ; then
+        echo "$CONDOR_JWT_TOKEN" > /etc/condor/tokens.d/JWT
+        chmod 600 /etc/condor/tokens.d/JWT
+    fi
 fi
