d22e0e1aba50d24f84bc423aa2b8f763fe873d87
bio-boris <bio-boris@users.noreply.github.com>
2023-01-13 13:41:43 -0600

    Update generate_ids.sh

--- generate_ids.sh
+++ generate_ids.sh
@@ -7,5 +7,5 @@ condor_token_create -identity rest_api@${UID_DOMAIN} -token rest_api
 condor_token_create -identity condor_stats@${UID_DOMAIN} -token condor_stats
 
 #Used in EE2 Submit
-condor_token_create -identity condor_pool@${UID_DOMAIN} -token kbase_submit
+condor_token_create -identity condor_pool@${UID_DOMAIN} -token condor_pool
 
