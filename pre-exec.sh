dd8c25e9663c1f63e9d30374e62f4e4029d35aca
Boris <bio-boris@github.com>
2023-02-07 14:23:30 -0600

    perms

--- pre-exec.sh
+++ pre-exec.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-############################################################################### 
+###############################################################################
 # See documentation at https://github.com/htcondor/htcondor/tree/main/build/docker/services#providing-additional-configuration
 # Condor Signing Key (Required to create future tokens) is automatically created upon starting this container
 # Once you have the key, you should provide it so it stays the same forever.
-############################################################################### 
+###############################################################################
 if [ "$CONDOR_SIGNING_KEY" ] ; then
     echo "$CONDOR_SIGNING_KEY" > /etc/condor/passwords.d/POOL
     chmod 600 /etc/condor/passwords.d/POOL
 fi
 
-############################################################################### 
+###############################################################################
 # Give specific JWT tokens permissions for Running Workers, Schedd, And Jobs
-############################################################################### 
+###############################################################################
 if [ "$UID_DOMAIN" ] ; then
     echo "ALLOW_ADVERTISE_STARTD = \$(ALLOW_ADVERTISE_STARTD) kbase_workers@${UID_DOMAIN} nersc_workers@${UID_DOMAIN}" >> /etc/condor/condor_config.local
-    echo "ALLOW_WRITE = \$(ALLOW_WRITE) kbase_workers@${UID_DOMAIN} nersc_workers@${UID_DOMAIN}" >> /etc/condor/condor_config.local  
+    echo "ALLOW_WRITE = \$(ALLOW_WRITE) kbase_workers@${UID_DOMAIN} nersc_workers@${UID_DOMAIN}" >> /etc/condor/condor_config.local
 fi
 
 if [ "$CONDOR_JWT_TOKEN" ] ; then
@@ -24,31 +24,31 @@ if [ "$CONDOR_JWT_TOKEN" ] ; then
 fi
 
 ####################### HOST PATHS ############################################
-#Ensure proper permissions for directories
 
-CONDOR_DIRS=$(condor_config_val LOCAL_DIR SPOOL LOG LOCAL_DIR LOCK CONDOR_SHARED | xargs)
+
+CONDOR_DIRS=$(condor_config_val LOCAL_DIR LOG SPOOL CONDOR_SHARED | xargs)
 mkdir -p $CONDOR_DIRS
-chmod 700 $CONDOR_DIRS
-
+chmod 770 $CONDOR_DIRS
+chown root:condor $CONDOR_DIRS
 
 /update-config
 ####################### HOST PATHS ############################################
 
 
 
-############################################################################### 
+###############################################################################
 # Overwrite the default config file that comes with this image
-############################################################################### 
+###############################################################################
 if [ "$OVERWRITE_CONFIG_FILEPATH" ] ; then
     cp "$CONDOR_CONFIG_FILEPATH" /etc/condor/condor_config.local
     /update-config
 fi
 
 
-############################################################################### 
+###############################################################################
 # FOR TESTING PURPOSES ONLY
 # SET COLLECTOR_HOST TO TURN THIS CONTAINER INTO A WORKER
-############################################################################### 
+###############################################################################
 if [ "$COLLECTOR_HOST" ] ; then
     echo "COLLECTOR_HOST = $COLLECTOR_HOST" >> /etc/condor/condor_config.local
     echo "DAEMON_LIST = MASTER STARTD" >> /etc/condor/condor_config.local
