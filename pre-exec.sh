6f9ffc1e29a3339e85d0fe4ae892cc2537216520
Boris <bio-boris@github.com>
2023-02-07 14:41:00 -0600

    Fixup creation

--- pre-exec.sh
+++ pre-exec.sh
@@ -28,7 +28,7 @@ fi
 
 DIRS_TO_CREATE=$(condor_config_val DIRS_TO_CREATE)
 mkdir -p $DIRS_TO_CREATE
-chmod 700 $DIRS_TO_CREATE && chown root:root $DIRS_TO_CREATE
+chmod 770 $DIRS_TO_CREATE && chown root:condor $DIRS_TO_CREATE
 
 /update-config
 ####################### HOST PATHS ############################################
